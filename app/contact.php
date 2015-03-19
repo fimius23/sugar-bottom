dd6773460dd26bd52648d042c899d24b1696004c
Ben Kramer <ben.j.kramer@gmail.com>
2015-03-19 06:18:36 -0500

    first commit

---
+++ app/contact.php
@@ -0,0 +1,44 @@
+<?php
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$formcontent="Name: $name\n\nEmail: $email\n\nMessage: $message";
+	
+	// Enter the email where you want to receive the notification when someone submit form
+	$recipient = "kramerlicsw@gmail.com";
+	
+	$subject = "See Soon! Contact Form";
+	
+	$mailheader = "From: $email\\r\\n";
+	$mailheader .= "Reply-To: $email\\r\\n";
+	$mailheader .= "MIME-Version: 1.0\\r\\n";
+	
+	$success = mail($recipient, $subject, $formcontent, $mailheader);
+	
+	if ($success == true){
+	
+?>
+	
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for your e-mail. I will contact you soon.');
+		window.location = "../index.html";
+	</script>
+	
+<?php
+	
+	} else {
+	
+?>
+
+    <script language="javascript" type="text/javascript">
+		alert('Message not sent. Please send me an e-mail to kramerlicsw@gmail.com');
+		window.location = "../index.html";
+    </script>
+	
+<?php
+
+    }
+	
+?>
---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+(function() {
+    'use strict';
+
+    var querySelector = document.querySelector.bind(document),
+        navdrawerContainer = querySelector('.navdrawer-container'),
+        body = document.body,
+        appbarElement = querySelector('.app-bar'),
+        main = querySelector('main'),
+        hamburger = querySelector('.hamburglar'),
+        hamburgerIsClosed = false;
+
+    function closeMenu() {
+        body.classList.remove('open');
+        appbarElement.classList.remove('open');
+        navdrawerContainer.classList.remove('open');
+    }
+
+    function toggleMenu() {
+        body.classList.toggle('open');
+        appbarElement.classList.toggle('open');
+        navdrawerContainer.classList.toggle('open');
+        navdrawerContainer.classList.add('opened');
+    }
+
+    function burgerTime() {
+        if (hamburgerIsClosed) {
+            hamburger.classList.remove('is-open');
+            hamburger.classList.add('is-closed');
+            hamburgerIsClosed = false;
+        } else {
+            hamburger.classList.remove('is-closed');
+            hamburger.classList.add('is-open');
+            hamburgerIsClosed = true;
+        }
+    }
+
+    main.addEventListener('click', closeMenu);
+    navdrawerContainer.addEventListener('click', function(event) {
+        if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+            closeMenu();
+        }
+    });
+    hamburger.addEventListener('click', burgerTime);
+    hamburger.addEventListener('click', toggleMenu);
+})();
